a5acd421bef0ed9ccb21dc0c6540a42fdbb2671f
Noran Mohamed <103522611+noran-mohamed@users.noreply.github.com>
2026-01-31 21:27:38 +0200

    Delete Solutions/700-799 directory

--- Solutions/700-799/744. Find Smallest Letter Greater Than Target.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for char in letters:
-            if char > target :
-                return char
-        return letters[0]
-
-
-        
