93461f27c71b6dff5671c0ce2dda1e5011dd5a4c
Noran Mohamed <103522611+noran-mohamed@users.noreply.github.com>
2026-02-06 22:43:00 +0200

    Create 3634. Minimum Removals to Balance Array.py

---
+++ Solutions/3600-3699/3634. Minimum Removals to Balance Array.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minRemoval(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        nums.sort()
+
+        ans = 0
+        j = 0
+        for i in range(n):
+            while j + 1 < n and nums[i] * k >= nums[j + 1]:
+                j += 1
+            print(j)
+            ans = max(ans, j - i + 1)
+
+        return n - ans
